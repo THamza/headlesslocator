deb240db71c29d7ba5fc3bdf927f82ff0a50f6f0
ThamzaSS <touhs.hamza@gmail.com>
2024-06-22 00:22:33 +0300

    working functionality

--- components/Header.tsx
+++ components/Header.tsx
@@ -23,7 +23,7 @@ const roboto = Roboto({
 
 export default function Header() {
   return (
-    <header className="flex items-center justify-between bg-primary px-4 py-3 shadow-sm">
+    <header className="flex items-center justify-between bg-primary px-4 py-3 shadow-sm bg-white">
       <div className="flex items-center">
         <Link href="https://headless.org/" prefetch={false} target="_blank">
           <Image
--- components/LeafletMap.tsx
+++ components/LeafletMap.tsx
@@ -1,5 +1,13 @@
 import { useEffect } from "react";
-import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
+import {
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer,
+  Circle,
+  useMap,
+} from "react-leaflet";
+import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 import "leaflet-defaulticon-compatibility";
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
@@ -12,9 +20,36 @@ function ChangeView({ center }: any) {
   return null;
 }
 
-export default function LeafletMap(props: any) {
-  const { position, zoom, onMarkerDragEnd } = props;
+// Create a custom div icon for other users
+const createCircularIcon = (color: string) => {
+  return new L.DivIcon({
+    html: `<div style="background-color: ${color}; width: 12px; height: 12px; border-radius: 50%; border: 2px solid white;"></div>`,
+    className: "",
+    iconSize: [12, 12],
+    iconAnchor: [6, 6], // Center the icon
+  });
+};
 
+// Create a circular icon for other users
+const otherUserIcon = createCircularIcon("green");
+
+// Default Leaflet icon for current user
+const currentUserIcon = L.icon({
+  iconUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png",
+  shadowUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png",
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41],
+});
+
+export default function LeafletMap({
+  position,
+  zoom,
+  radius,
+  fixedMarker = false,
+  users = [],
+}: any) {
   return (
     <MapContainer
       center={position}
@@ -29,19 +64,37 @@ export default function LeafletMap(props: any) {
       <ChangeView center={position} />
       <Marker
         position={position}
-        draggable={true}
-        eventHandlers={{
-          dragend: (event) => {
-            const latlng = event.target.getLatLng();
-            onMarkerDragEnd(latlng);
-          },
-        }}
+        draggable={!fixedMarker}
+        icon={currentUserIcon}
       >
         <Popup>
           Your location <br /> {position.lat.toFixed(3)},{" "}
           {position.lng.toFixed(3)}
         </Popup>
       </Marker>
+      {radius && (
+        <Circle
+          center={position}
+          radius={radius * 1000} // Convert radius to meters
+          fillColor="blue"
+          color="blue"
+        />
+      )}
+      {users.map((user: any) => (
+        <Marker
+          key={user.id}
+          position={{ lat: user.latitude, lng: user.longitude }}
+          icon={otherUserIcon}
+        >
+          <Popup>
+            {user.firstName} {user.lastName}
+            <br />
+            {user.email}
+            <br />
+            {user.telegram}
+          </Popup>
+        </Marker>
+      ))}
     </MapContainer>
   );
 }
